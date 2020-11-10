afeb18883c95a71536aae3fb2b2dcf26ead33b83
runes224 <hitakarento@gmail.com>
2020-11-11 07:48:19 +0900

    To3

--- 300/To 3.cpp
+++ 300/To 3.cpp
@@ -5,31 +5,24 @@ const int INF = 1001001001;
 
 int main()
 {
-  int N;
+  string N;
   cin >> N;
-  int tmpN = N;
-  vector<int> a = {N};
+  vector<int> a(3);
+  rep(i, N.size()) a[(N[i] - '0') % 3]++;
   int ans = INF;
-  rep(i, to_string(tmpN).length()-1)
+  int cur = ((a[1] * 1 + a[2] * 2) % 3);
+  if (cur == 0) ans = 0;
+  else if (cur == 1)
   {
-    vector<int> tmpA = {};
-    for (int ele : a)
-    {
-      string str = to_string(ele);
-      if (ele % 3 == 0) ans = min(ans, i);
-      if (str.length() == 1) continue;
-      string tmpStr = str;
-      rep(j, tmpStr.length())
-      {
-        int tmp;
-        string tmpStr2 = str;
-        tmp = stoi(tmpStr2.erase(j, 1));
-        if (tmp % 3 == 0) tmpA.push_back(tmp);
-      }
-    }
-    a.clear();
-    a = tmpA;
+    if (a[1] >= 1 && N.size() > 1) ans = 1;
+    else if (a[2] >= 2 && N.size() > 2) ans = 2;
+    else ans = -1;
   }
-  if (ans == INF) ans = -1;
+  else
+  {
+    if (a[2] >= 1 && N.size() > 1) ans = 1;
+    else if (a[1] >= 2 && N.size() > 2) ans = 2;
+    else ans = -1;
+  }
   cout << ans << endl;
 }
