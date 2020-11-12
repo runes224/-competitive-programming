2da91b5ce220bb2bb4bc39d1912ad78a7c97733f
runes224 <hitakarento@gmail.com>
2020-11-12 09:33:57 +0900

    box

---
+++ 100/box.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, c) for (int i = 0; i < (int)c; i++)
+const int INF = 1001001001;
+
+int main()
+{
+  int N, A, B;
+  cin >> N >> A >> B;
+  cout << (N-A+B) << endl;
+}
